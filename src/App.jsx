8d5e97f9265dc3f3312bbafb29aa4c0fb4cacad5
Hibatul Muqeet <161466064+HibatulM@users.noreply.github.com>
2026-04-19 19:35:47 -0500

    adding assets
    Adding new assets to project

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import './App.css'
 
 const App  = () => {
   return (
-    <body>
+    <body className="bg-gray-900">
       <header>
-        <nav>
-          <ul className="">
-            <li>Home</li>
-            <li>Services</li>
-            <li>Projects</li>
-            <li>About</li>
-            <li>Contact Me</li>
+        <nav className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <ul className="flex justify-center
+             px-1.5 py-2.5 text-amber-50
+            gap-1.5 bg-linear-to-tr from-pink-950 to-pink-500
+           ">
+            <li><img src="/src/assets/web-logo.png" className="mask-auto mx-0">
+            </img></li>
+            <li className="text-1.5xl" >Home</li>
+            <li className="text-1.5xl">Projects</li>
+            <li className="text-1.5xl">About</li>
           </ul>
         </nav>
       </header>
